c394e69ae748c8f541b97176eaf19168be1ba93c
José Luis Tello <jltello96@gmail.com>
2022-11-02 00:48:00 -0600

    adds

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/')
-def index():
-    return 'Hello World'
+
+@app.get('/blog/all')
+def get_all_blogs():
+    return {'message': 'All blogs provided'}
+
+@app.get('/blog/{id}')
+def get_blog(id: int):
+    return {"message": f"Blog with id {id}"}
