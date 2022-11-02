30cd24e36257fa64e8e6ff2892811d07f53d8c2a
José Luis Tello <jltello96@gmail.com>
2022-11-02 01:54:01 -0600

    adds

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from fastapi import FastAPI
 from enum import Enum
 
@@ -5,9 +6,30 @@ from enum import Enum
 app = FastAPI()
 
 
-@app.get('/blog/all')
-def get_all_blogs():
-    return {'message': 'All blogs provided'}
+#@app.get('/blog/all')
+#def get_all_blogs():
+#    return {'message': 'All blogs provided'}
+
+
+# Default values V 
+
+#@app.get('/blog/all')
+#def get_all_blogs(page = 1, page_size = 10):
+#    return ('message': f'All {page_size} blogs on page {page}')
+
+
+# Optional Parameters
+
+#@app.get('/blog/all')
+#def get_all_blogs(page = 1, page_size: Optional[str] = None):
+#    return ('message': f'All {page_size} blogs on page {page}')
+
+
+## Complex 
+@app.get('/blog/{id}/comments/{comment_id}')
+def get_comment(id: int, comment_id: int, valid: bool = True, username: Optional[str] = None):
+    return {'message': f'blog_id {id}, comment_id {comment_id}, valid {valid}, username {username}'}
+
 
 
 class BlogType(str, Enum):
