564a4f4804fbe128caf940b2342e468d1b9e0c25
José Luis Tello <jltello96@gmail.com>
2022-11-02 01:12:27 -0600

    adds

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from enum import Enum
 
 
 app = FastAPI()
@@ -7,6 +8,17 @@ app = FastAPI()
 @app.get('/blog/all')
 def get_all_blogs():
     return {'message': 'All blogs provided'}
+
+
+class BlogType(str, Enum):
+    short = 'short'
+    story = 'story'
+    howto = 'howto'
+
+@app.get('/blog/type/{type}')
+def get_blog_type(type: BlogType):
+    return {'message': f'Blog type {type}'}
+
 
 @app.get('/blog/{id}')
 def get_blog(id: int):
